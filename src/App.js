827948f77b095ed2e9d5c6347890907ff6ced0e0
bahareh-bahrami <bahareh.bahramy@gmail.com>
2025-09-21 16:12:56 +0330

    Add logo component

---
+++ src/App.js
@@ -0,0 +1,29 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 1, description: "Charger", quantity: 1, packed: true },
+  { id: 2, description: "Sun Glasses", quantity: 1, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+
+function Form() {}
+
+function PackingList() {}
+
+function Item() {}
+
+function Stats() {}
